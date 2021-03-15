9b0604307a2d23e66ed772f2497d6783366000c5
Aurelien <aurelien.isak@epita.fr>
2021-03-15 18:54:27 +0000

    Try new Solution

---
+++ Dublin Bikes/Dublin Bikes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dublin_Bikes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
