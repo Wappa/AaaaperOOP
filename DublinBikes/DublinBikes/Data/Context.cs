ee01c3edd3c1f544a9b356a9371501d241dfcc68
Yanis Hermassi <yanis.hermassi@gmail.com>
2021-03-21 14:00:22 +0000

    add context

---
+++ DublinBikes/DublinBikes/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DublinBikes.Models;
+
+
+namespace DublinBikes.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<BikeStation> BikeStation { get; set; }
+    }
+}
